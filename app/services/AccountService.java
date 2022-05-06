aae309bbcd521181edc0f5ff1dce85750f5ab9a8
Adam Wight <adam.wight@wikimedia.de>
2022-05-06 12:19:52 +0200

    Tear out keycloak
    TODO: Rewrite AccountManagers, don't assume admin access to change
    facts

--- app/services/AccountService.java
+++ app/services/AccountService.java
@@ -1,11 +1,6 @@
 package services;
 
 public interface AccountService {
-  void setPermissions(String aId, String aUser);
-  boolean removePermissions(String aId);
-  void setApache2Ctl(String aApache2ctl); // FIXME: remove from interface
-  boolean deleteUser(String username);
   String getProfileId(String username);
-  void setProfileId(String username, String profileId);
   String getUsername(String profileId);
 }
